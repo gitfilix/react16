18380029fba886b24462ea7d5b967e32082121a6
gitfilix <runlevel07@gmail.com>
2018-04-16 14:43:04 +0200

    pass methods to dump rendering components as props

--- myflix-app/src/App.js
+++ myflix-app/src/App.js
@@ -9,21 +9,43 @@ class App extends Component {
             {name: 'Manuel', age: 27},
             {name: 'Stephanie', age: 37},
             {animal: 'dog', color: 'black'}
-        ]
+        ],
+        otherState: 'some other string-values'
     }
 
-    switchNameHandler = () => {
-        console.log("clicked dude!");
+    switchNameHandler = (newName) => {
+        console.log("switchNameHandler this", this);
+        console.log("switchNameHandler newName", newName);
+        this.setState( {
+            persons : [
+                {name: newName, age: 28},
+                {name: 'Emanuelle', age: 17},
+                {name: 'Stephanie', age: 47},
+                {animal: 'dog', color: 'black'}
+            ]
+        })
     }
 
   render() {
     return (
       <div className="App">
         <h2>Hi! I am Flix React App</h2>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>and I have no hobbies</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        exec handler with bind:
+        <button onClick={this.switchNameHandler.bind(this, 'filix')}>Switch Name</button>
+        <br />
+        exec handler with arrow anonimous function & call it !
+        <button onClick={ ()=> this.switchNameHandler('filix!!')}>Switch Name</button>
+        <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}>and I have no hobbies</Person>
+        <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            // pass a function or method as props to the function component- nice
+            click={this.switchNameHandler.bind(this, 'Max!')}>and I like to click</Person>
+        <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
       </div>
     );
   }
--- myflix-app/src/Person/Person.js
+++ myflix-app/src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+// this is a functional component - stateless and dump for presentational reason only
 
 // es6 fn: argument-list, arrow, funcion-body
 const person = (props) => {
     return (
         <div>
-            <p>I am {props.name} and I am {props.age} years old. </p>
+            <p onClick={props.click}>I am {props.name} and I am {props.age} years old. </p>
         </div>
     )
 }
