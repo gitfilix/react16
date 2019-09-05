553b79f660cb03e55e513af4da95c2fab9905043
gitfilix <>
2019-09-05 17:12:02 +0200

    react hooks: store ingredients into firebase

--- hooks/src/components/Ingredients/IngredientForm.js
+++ hooks/src/components/Ingredients/IngredientForm.js
@@ -15,7 +15,10 @@ const IngredientForm = React.memo(props => {
 
   const submitHandler = event => {
     event.preventDefault();
-    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
+    props.onAddIngredient({
+      title: enteredTitle,
+      amount: enteredAmount
+    });
   };
 
   return (
--- hooks/src/components/Ingredients/Ingredients.js
+++ hooks/src/components/Ingredients/Ingredients.js
@@ -5,15 +5,26 @@ import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
-    const [ userIngredients, setUserIngredients] = useState([]);
+    const [ userIngredients, setUserIngredients ] = useState([]);
 
     // handler for the form to add some ingr. called by Ing-form submitHandler onAddIngredient.
     const addIngredientHandler = (ingredient) => {
-        // get most recent Ingredient('prevIngredients') and
-        setUserIngredients(prevIngredients => [
-            ...prevIngredients,
-            { id: Math.random().toString(), ...ingredient}
-        ]);
+        // firebase connection as POST req with headers config
+        fetch('https://react-hooks-5b94f.firebaseio.com/ingredients.json', {
+          method: 'POST',
+          body: JSON.stringify(ingredient),
+          headers: { 'Content-Type' : 'application/json' }
+        }).then(response => {
+            return response.json();
+        }).then(responseData => {
+            // get most recent Ingredient('prevIngredients') and
+            // responseData comes from firebase .name property is firebase id convention
+          setUserIngredients(prevIngredients => [
+              // update previous data
+              ...prevIngredients,
+              { id: responseData.name, ...ingredient }
+            ]);
+        });
     };
 
     return (
