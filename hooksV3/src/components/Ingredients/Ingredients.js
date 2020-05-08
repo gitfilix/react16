3266c08ce4fddc9304b9e60555a12e01d3a27bf1
gitfilix <runlevel07@gmail.com>
2020-05-08 12:39:41 +0200

    useReducer

--- hooksV3/src/components/Ingredients/Ingredients.js
+++ hooksV3/src/components/Ingredients/Ingredients.js
@@ -1,11 +1,28 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useReducer } from 'react'
 import IngredientList from './IngredientList'
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from './IngredientForm'
+import Search from './Search'
 import ErrorModal from '../UI/ErrorModal'
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      // return all ing exept the one specified - that one is deleted.
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('reducer: no action defined')
+  }
+}
+
 const Ingredients= () => {
-  const [userIngredients, setUserIngredients] = useState([])
+  // initialize useReducer with data (userIngredients)
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+
+  // const [userIngredients, setUserIngredients] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState() // no initial value
 
@@ -29,10 +46,17 @@ const Ingredients= () => {
       // exexutes if fetch-promise is resolved
       // responseData is Firebase convention
     }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients, 
-        { id: responseData.name, ...ingredient }
-      ])
+      //setUserIngredients(prevIngredients => [
+      //  ...prevIngredients, 
+      //  { id: responseData.name, ...ingredient }
+      //])
+      dispatch({
+        type: 'ADD',
+        ingredient: { 
+          id: responseData.name, 
+          ...ingredient 
+        }
+      })
     }).catch(error => {
       setError('an error occured!')
       setIsLoading(false)
@@ -41,7 +65,11 @@ const Ingredients= () => {
   
   // filteredIngredients on search component with useCallback wrapper
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-     setUserIngredients(filteredIngredients)
+    //  setUserIngredients(filteredIngredients)
+    dispatch({
+      type: 'SET',
+      ingredients: filteredIngredients
+    })
   }, [])
 
   // delete item
@@ -52,8 +80,12 @@ const Ingredients= () => {
       method: 'DELETE'
     }).then(response => {
       setIsLoading(false)
-      setUserIngredients((prevIngredients) => 
-        prevIngredients.filter((ingredient) => (ingredient.id !== ingredientId) ))
+      // setUserIngredients((prevIngredients) => 
+        // prevIngredients.filter((ingredient) => (ingredient.id !== ingredientId) ))
+      dispatch({
+        type: 'DELETE',
+        id: ingredientId
+      })  
     })
   }
 
